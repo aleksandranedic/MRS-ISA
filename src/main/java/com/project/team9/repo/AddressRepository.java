49cf67a2d5c4c3c5e5b2080d755b996a617b172f
dunjastojanov <61994882+dunjastojanov@users.noreply.github.com>
2022-04-20 21:04:37 +0200

    Revert "feature-edit-adventure"

--- src/main/java/com/project/team9/repo/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.team9.repo;
-
-import com.project.team9.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
