d1d48acc49224b35a65710e1ffcd2ad2605d752d
Dunja Stojanov <dunjastojanov2@gmail.com>
2022-05-08 14:32:47 +0200

    vacation house card and carousel

---
+++ src/main/frontend/src/VacationHousePage/AllVacationHouses.js
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {backLink} from "../Consts";
+
+export function GetAllVacationHouses() {
+    const [vacationHouses, setVacationHouses] = useState([]);
+
+    const fetch = () => {
+        axios.get(backLink + "house").then(res => {
+            console.log(res.data);
+            setVacationHouses(res.data);
+        });
+    };
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
+    return vacationHouses;
+}
