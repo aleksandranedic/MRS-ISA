f4db4f7252278d3784350b04cbefec6a4982b22f
Dunja Stojanov <dunjastojanov2@gmail.com>
2022-05-10 01:35:07 +0200

    25.0.1.1 Napraviti stranicu sa prikazom zahteva za registraciju pružaoca usluga

--- src/main/frontend/src/Admin/RegistrationRequests.js
+++ src/main/frontend/src/Admin/RegistrationRequests.js
@@ -4,42 +4,50 @@ import {RegistrationRequestCard} from "./RegistrationRequestCard";
 import {Button, Modal} from "react-bootstrap";
 export function RegistrationRequests() {
 
+    const [requests, setRequests] = useState([
+        {
+            name: "Marko Jovanovic",
+            type: "Instruktor pecanja",
+            email: "marecare@email.com",
+            phoneNumber: "0659632147",
+            address: {
+                street: "Pere Dobrinovica",
+                number: "25",
+                country: "Srbija",
+                place: "Novi Sad"
+            }
+        },
+        {
+            name: "Sinisa Misic",
+            type: "Vlasnik vikendice",
+            email: "siiznisa@email.com",
+            phoneNumber: "0645832147",
+            address: {
+                street: "Pere Dobrinovica",
+                number: "25",
+                country: "Srbija",
+                place: "Novi Sad"
+            }
+        }
 
+
+    ])
+
+    const remove = (request) => {
+        setRequests(requests.filter(r => r !== request))
+    }
 
     return (<div className="d-flex"  style={{height: "100vh"}}>
         <div className="w-25" style={{backgroundColor: "#f7f8f9"}}>
             <Sidebar/>
         </div>
         <div className="w-75 overflow-auto">
-            <RegistrationRequestCard props={
-                {
-                    name: "Marko Jovanovic",
-                    type: "Instruktor pecanja",
-                    email: "marecare@email.com",
-                    phoneNumber: "0659632147",
-                    address: {
-                        street: "Pere Dobrinovica",
-                        number: "25",
-                        country: "Srbija",
-                        place: "Novi Sad"
-                    }
-                }
-            }/>
-            <RegistrationRequestCard props={
-                {
-                    name: "Sinisa Misic",
-                    type: "Vlasnik vikendice",
-                    email: "siiznisa@email.com",
-                    phoneNumber: "0645832147",
-                    address: {
-                        street: "Pere Dobrinovica",
-                        number: "25",
-                        country: "Srbija",
-                        place: "Novi Sad"
-                    }
-                }
-            }/>
 
+            {requests.map(request => {
+                return (
+                    <RegistrationRequestCard props={request} remove={remove}/>
+                )
+            })}
 
         </div>
     </div>)
