851683c78b75ec9ad1ba1c0ba13d61b3c0c532d2
Milos Milovanovic <milosmilovanovic1@yahoo.com>
2022-05-09 18:30:40 +0200

    #28 0.0.0.3 Modifikacija klasa preko koje se prosledjuju username i password backendu

--- src/main/java/com/project/team9/dto/LoginDTO.java
+++ src/main/java/com/project/team9/dto/LoginDTO.java
@@ -1,14 +1,22 @@
 package com.project.team9.dto;
 
 public class LoginDTO {
-    private String email;
+    private String username;
     private String password;
     public LoginDTO() {
     }
 
-    public LoginDTO(String email, String password) {
-        this.email = email;
+    public LoginDTO(String username, String password) {
+        this.username = username;
         this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -17,13 +25,5 @@ public class LoginDTO {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
